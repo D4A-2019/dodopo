8938822d26b19c8b38d5897d8e678ad0e2f28524
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-08-16 03:33:00 +0700

    refactor: productCard component props

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import ProductCard from '../components/product-card/product-card'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-      
-        <div className={styles.elwrapper}>
-          <div className={styles.boxup}>
-            <img className={styles.img} src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" alt=""></img>
-            <div className={styles.imginfo}>
-              <div className={styles.infoinner}>
-                <span className={styles.pname}>I feel like Pablo</span>
-                <span className={styles.pcompany}>Yeezy</span>
-              </div>
-              <div className={styles.asize}>Available sizes : <span className={styles.size}>S , M , L , XL</span></div>
-            </div>
-          </div>
-
-          <div className={styles.boxdown}>
-            <span className={styles.price}>$129</span>
-            <a className={styles.cart} href="#">
-              <span className={styles.addtocart}>
-                <span className={styles.txt}>Add to cart</span>
-              </span>
-            </a>
-          </div>
+            <main className={styles.main}>
+                <ProductCard 
+                    imagesrc="http://code.slicecrowd.com/labs/4/images/t-shirt.png"
+                    productName="I feel like Pablo"
+                    productSeller="Yeezy"
+                    sizes="S , M , L , XL"
+                    price="$129"
+                />
+            </main>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
